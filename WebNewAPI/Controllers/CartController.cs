0f0513748df7236f3538c3fba494f1417e923723
perusha <perushawanniarachchi22@gmail.com>
2022-05-17 19:50:39 +0530

    initial commit

---
+++ WebNewAPI/Controllers/CartController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using WebNewAPI.Model;
+
+namespace WebNewAPI.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+        private static List<CartModel> CartObjs;
+        public CartController()
+        {
+            CartObjs = new List<CartModel>();
+        }
+
+        [HttpPost]
+        [Route("Addcart")]
+        public string SaveCart([FromBody]CartModel cartModel)
+        {
+            
+            string fileName = "json.json";
+
+
+            string jsonStringr = System.IO.File.ReadAllText(fileName);
+            List<CartModel> weatherForecast = JsonSerializer.Deserialize<List<CartModel>>(jsonStringr)!;
+            weatherForecast.Add(cartModel);
+            string jsonString = JsonSerializer.Serialize(weatherForecast);
+            System.IO.File.WriteAllText(fileName, jsonString);
+
+
+            return "Saved";
+
+        }
+
+        [HttpGet]
+        [Route("Showcart")]
+        public List<CartModel> ShowCart()
+        {
+            string fileName = "json.json";
+
+
+            string jsonStringr = System.IO.File.ReadAllText(fileName);
+            List<CartModel> weatherForecast = JsonSerializer.Deserialize<List<CartModel>>(jsonStringr)!;
+            return weatherForecast;
+        }
+    }
+}
---
+++ WebNewAPI/Model/CartModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebNewAPI.Model
+{
+    public class CartModel
+    {
+        public string  itemID { get; set; }
+        public string itamName { get; set; }
+        public string itamCategory { get; set; }
+        public string quantity { get; set; }
+        public string description { get; set; }
+    }
+}
